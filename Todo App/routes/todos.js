f39a2ed1c9caade13e743deaaba2f6ba874118d2
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-06-21 18:11:39 +0530

    getTodo api continue...

--- Todo App/routes/todos.js
+++ Todo App/routes/todos.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // import controler 
 const {createTodo} = require("../controllers/createTodo");
+const {getTodos} = require("../controllers/getTodos");
 
 
 // define api route 
 router.post("/createTodo",createTodo);
+router.get("/getTodos",getTodos);
 
 module.exports = router;
