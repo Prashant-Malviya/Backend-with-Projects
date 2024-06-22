9d378439ca9284e0893457dc3a5dfa4bd65744f1
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-06-22 11:13:09 +0530

    todo backend built completed

--- Todo App/routes/todos.js
+++ Todo App/routes/todos.js
@@ -4,10 +4,16 @@ const router = express.Router();
 // import controler 
 const {createTodo} = require("../controllers/createTodo");
 const {getTodos} = require("../controllers/getTodos");
+const {getTodoById} = require("../controllers/getTodos");
+const {updateTodo} = require("../controllers/getTodos");
+const {deleteTodos} = require("../controllers/getTodos");
 
 
 // define api route 
 router.post("/createTodo",createTodo);
 router.get("/getTodos",getTodos);
+router.get("/getTodos/:id",getTodoById);
+router.put("/updateTodo/:id",updateTodo);
+router.delete("/deleteTodos/:id",deleteTodos);
 
 module.exports = router;
