e40abdf0c5f93c58a086b186cd396e6776a41ce7
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-08-26 01:33:37 +0530

    deleteTodo feature added

--- Todo App/routes/todo.js
+++ Todo App/routes/todo.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const {createTodo} = require("../controllers/create-todo");
 const {getTodo} = require("../controllers/get-todo");
 const { getTodoById } = require("../controllers/get-todo-by-id");
+const { deleteTodo } = require("../controllers/delete-todo");
 
 //define api routes
 router.post("/createTodo",createTodo);
 router.get("/getTodos",getTodo);
-router.get("/getTodos/:id",getTodoById)
+router.get("/getTodos/:id",getTodoById);
+router.delete("/deleteTodo/:id",deleteTodo);
 
 module.exports = router;
